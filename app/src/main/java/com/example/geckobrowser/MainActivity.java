598373321d99a5829e02078b5642fae9137a72b4
mdiori <mdiori@hotmail.com>
2021-03-05 14:14:43 -0300

    Removed status bar

--- app/src/main/java/com/example/geckobrowser/MainActivity.java
+++ app/src/main/java/com/example/geckobrowser/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.geckobrowser;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,13 +12,20 @@ import org.mozilla.geckoview.GeckoSessionSettings;
 import org.mozilla.geckoview.GeckoView;
 
 public class MainActivity extends AppCompatActivity {
-    public String INITIAL_URL = "https://start.duckduckgo.com";
+    public String INITIAL_URL = "192.168.1.2";
     private GeckoView mGeckoView;
     private GeckoSession mGeckoSession;
     private GeckoSessionSettings.Builder settingsBuilder = new GeckoSessionSettings.Builder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
